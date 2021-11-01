74ecdd6b452a57d47d9b715dbbc90b440e2d645d
Gabriel Pavan <gabrielpavan2014@outlook.com>
2021-11-01 16:29:33 -0300

    Donut Update

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -8,7 +8,10 @@ const DonutChart = () => {
     const options = {
         legend: {
             show: true
-        }
+        },
+        fill: {
+            type: 'gradient',
+        },
     }
     return (
         <Chart
