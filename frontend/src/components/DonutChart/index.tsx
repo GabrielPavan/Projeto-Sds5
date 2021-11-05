3d9b4576ed43289d2774bca2f8c58dfde2f6eb84
Gabriel Pavan <gabrielpavan2014@outlook.com>
2021-11-05 13:25:18 -0300

    BarChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -21,14 +21,6 @@ const DonutChart = () => {
                 setChartData({ labels: myLabels, series: mySeries });
             });
     }, []);
-
-    //Forma errada
-
-
-    //const mockData = {
-    //    series: [477138, 499928, 444867, 220426, 473088],
-    //    labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    //}
     const options = {
         legend: {
             show: true
--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,4 +1,9 @@
 export type SaleSum = {  
-    sellername: string,
-    sum: number
+    sellername: string;
+    sum: number;
+}
+export type SaleSuccess = {
+    sellername: string;
+    visited: number;
+    deals: number;
 }
